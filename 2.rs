f32a1dad0241e7e31fd3207edfa56be255305568
Jay Rane <127013993+Jay-shm@users.noreply.github.com>
2026-01-17 21:58:27 +0530

    first time

---
+++ 2.rs
@@ -0,0 +1,6 @@
+// print hello world and goodbye by 2 diffrent print method in rust
+
+fn main(){
+    println!("Hello World!"); // using println -> adds a newline
+    print!("Goodbye!"); // using print -> no newline
+}
